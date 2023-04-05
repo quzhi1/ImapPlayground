eb1564ba3104b2561d8d1bffaa88a92d7cbc2a5f
Zhi Qu <quzhi65222714@gmail.com>
2023-04-05 13:00:02 -0700

    Example has issues

---
+++ benchmark/v2_example/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/emersion/go-imap/v2"
+	"github.com/emersion/go-imap/v2/imapclient"
+)
+
+func main() {
+	// Read auth
+	username := os.Getenv("YAHOO_EMAIL_ADDRESS")
+	password := os.Getenv("YAHOO_APP_PASSWORD")
+
+	// Connect
+	c, err := imapclient.DialTLS("imap.mail.yahoo.com:993", nil)
+	if err != nil {
+		log.Fatalf("failed to dial IMAP server: %v", err)
+	}
+	defer c.Close()
+
+	// Login
+	if err := c.Login(username, password).Wait(); err != nil {
+		log.Fatalf("failed to login: %v", err)
+	}
+
+	// List mailboxes
+	mailboxes, err := c.List("", "%", nil).Collect()
+	if err != nil {
+		log.Fatalf("failed to list mailboxes: %v", err)
+	}
+	log.Printf("Found %v mailboxes", len(mailboxes))
+	for _, mbox := range mailboxes {
+		log.Printf(" - %v", mbox.Mailbox)
+	}
+
+	// Select INBOX
+	selectedMbox, err := c.Select("INBOX").Wait()
+	if err != nil {
+		log.Fatalf("failed to select INBOX: %v", err)
+	}
+	log.Printf("INBOX contains %v messages", selectedMbox.NumMessages)
+
+	// Fetch first message in INBOX
+	if selectedMbox.NumMessages > 0 {
+		seqSet := imap.SeqSetNum(1)
+		fetchItems := []imap.FetchItem{imap.FetchItemEnvelope}
+		messages, err := c.Fetch(seqSet, fetchItems).Collect()
+		if err != nil {
+			log.Fatalf("failed to fetch first message in INBOX: %v", err)
+		}
+		log.Printf("subject of first message in INBOX: %v", messages[0].Envelope.Subject)
+	}
+
+	// Logout
+	if err := c.Logout().Wait(); err != nil {
+		log.Fatalf("failed to logout: %v", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,15 +4,16 @@ go 1.19
 
 require (
 	github.com/emersion/go-imap v1.2.1
-	github.com/emersion/go-message v0.15.0
+	github.com/emersion/go-imap/v2 v2.0.0-alpha.2
+	github.com/emersion/go-message v0.16.0
 	github.com/rs/zerolog v1.29.0
 )
 
 require (
-	github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21 // indirect
+	github.com/emersion/go-sasl v0.0.0-20220912192320-0145f2c60ead // indirect
 	github.com/emersion/go-textwrapper v0.0.0-20200911093747-65d896831594 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
 )
