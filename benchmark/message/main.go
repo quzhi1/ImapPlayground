b4cdd4f031a654715afe51f2e1fe4f30f1c59915
Zhi Qu <quzhi65222714@gmail.com>
2023-05-10 18:24:19 -0700

    Play with draft attachment

--- benchmark/message/main.go
+++ benchmark/message/main.go
@@ -5,21 +5,36 @@ import (
 	"crypto/tls"
 	"io"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 
 	"github.com/emersion/go-imap"
 	"github.com/emersion/go-imap/client"
+	"github.com/emersion/go-message"
 	"github.com/emersion/go-message/mail"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
+var (
+	contentTypeRegex = regexp.MustCompile(".*name=\"(\\S+)\"")
+	// username = os.Getenv("YAHOO_EMAIL_ADDRESS")
+	// password = os.Getenv("YAHOO_APP_PASSWORD")
+	username = os.Getenv("ICLOUD_EMAIL_ADDRESS")
+	password = os.Getenv("ICLOUD_APP_PASSWORD")
+)
+
 const (
 	// imapAddress = "imap.mail.yahoo.com:993"
 	imapAddress = "imap.mail.me.com:993"
-	folderName  = "Sent Messages"
-	syncPeriod  = 30 * 24 * time.Hour
+	folderName  = "Hello"
+	// folderName           = "Drafts"
+	syncPeriod           = 30 * 24 * time.Hour
+	HTMLContentType      = "text/html"
+	PlainTextContentType = "text/plain"
+	uid                  = 6
+	// uid = 10
 )
 
 func main() {
@@ -31,12 +46,6 @@ func main() {
 		Logger().
 		Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	ctx := logger.WithContext(context.Background())
-
-	// Read auth
-	// username := os.Getenv("YAHOO_EMAIL_ADDRESS")
-	// password := os.Getenv("YAHOO_APP_PASSWORD")
-	username := os.Getenv("ICLOUD_EMAIL_ADDRESS")
-	password := os.Getenv("ICLOUD_APP_PASSWORD")
 
 	// Connect to imap server
 	log.Ctx(ctx).Debug().Msgf("Connecting to IMAP server %s", imapAddress)
@@ -64,23 +73,10 @@ func main() {
 	}
 	log.Ctx(ctx).Debug().Str("folderName", folderName).Uint32("UIDVALIDITY", folder.UidValidity).Msg("Selected folder")
 
-	// Get message UIDs
-	log.Ctx(ctx).Debug().Float64("syncPeriod", syncPeriod.Seconds()).Msg("Get message UIDs")
-	criteria := imap.NewSearchCriteria()
-	criteria.Since = time.Now().Add(-syncPeriod)
-	uids, err := imapClient.UidSearch(criteria)
-	if err != nil {
-		panic(err)
-	}
-
 	// Get messages
-	if len(uids) == 0 {
-		log.Ctx(ctx).Debug().Msg("No messages to fetch")
-		return
-	}
-	log.Ctx(ctx).Debug().Msgf("Fetching messages: %v", uids)
+	log.Ctx(ctx).Debug().Msgf("Fetching message: %v", uid)
 	seqset := new(imap.SeqSet)
-	seqset.AddNum(uids...)
+	seqset.AddNum(uid)
 	// Setting peek to true will prevent marking the messages as read
 	section := imap.BodySectionName{Peek: true}
 	items := []imap.FetchItem{
@@ -133,31 +129,28 @@ func main() {
 
 		// Process each message's part
 		for {
-			p, err := mr.NextPart()
+			part, err := mr.NextPart()
 			if err == io.EOF {
 				break
 			} else if err != nil {
 				panic(err)
 			}
 
-			switch h := p.Header.(type) {
+			switch partHeader := part.Header.(type) {
 			case *mail.InlineHeader:
-				log.Ctx(ctx).Info().Msgf("Got text Content-Type: %s", h.Header.Header.Get("Content-Type"))
-				// This is the message's text (can be plain-text or HTML)
-				b, _ := io.ReadAll(p.Body)
-				log.Ctx(ctx).Info().Msgf("Got text: %v", string(b))
-			case *mail.AttachmentHeader:
-				// This is an attachment
-				filename, _ := h.Filename()
-				log.Ctx(ctx).Info().Msgf("Got attachment: %v", filename)
-
-				// Print attachment
-				buf := new(strings.Builder)
-				_, err := io.Copy(buf, p.Body)
-				if err != nil {
-					panic(err)
+				contentType := partHeader.Header.Header.Get("Content-Type")
+				switch {
+				case strings.Contains(contentType, PlainTextContentType):
+					textBody := readerToString(ctx, part.Body)
+					log.Ctx(ctx).Debug().Str("text", textBody).Msg("Found text body")
+				case strings.Contains(contentType, HTMLContentType):
+					htmlBody := readerToString(ctx, part.Body)
+					log.Ctx(ctx).Debug().Str("html", htmlBody).Msg("Found html body")
+				default:
+					printAttachment(ctx, partHeader.Header, part)
 				}
-				log.Ctx(ctx).Info().Msgf("Attachment: %s", buf.String())
+			case *mail.AttachmentHeader:
+				printAttachment(ctx, partHeader.Header, part)
 			}
 		}
 	}
@@ -185,4 +178,60 @@ func listFolder(ctx context.Context, client *client.Client) []imap.MailboxInfo {
 		result = append(result, *m)
 	}
 	return result
+}
+
+func printAttachment(ctx context.Context, header message.Header, part *mail.Part) {
+	contentType := header.Header.Get("Content-Type")
+
+	// Get or generate file ID
+	var fileID string
+	if header.Header.Has("X-Attachment-Id") {
+		fileID = header.Header.Get("X-Attachment-Id")
+	} else {
+		panic("Attachment has no X-Attachment-Id header")
+	}
+
+	// Read file name
+	var fileName string
+	match := contentTypeRegex.FindStringSubmatch(contentType)
+	if match != nil {
+		// Extract the captured string from the first submatch
+		fileName = match[1]
+	} else {
+		log.Ctx(ctx).Warn().
+			Str("content_type", contentType).
+			Str("file_id", fileID).
+			Msg("Attachment has file name in Content-Type header, setting it to empty")
+	}
+
+	// Read content disposition
+	var contentDisposition string
+	if header.Header.Has("Content-Disposition") {
+		contentDisposition = header.Header.Get("Content-Disposition")
+	} else {
+		log.Ctx(ctx).Warn().
+			Str("content_type", contentType).
+			Str("file_id", fileID).
+			Msg("Attachment has no Content-Disposition header, setting it to empty")
+	}
+
+	// Read file content
+	content := readerToString(ctx, part.Body)
+	log.Ctx(ctx).Debug().
+		Str("content_type", contentType).
+		Str("file_id", fileID).
+		Str("file_name", fileName).
+		Str("content_disposition", contentDisposition).
+		Str("content", content).
+		Msg("Found attachment")
+}
+
+func readerToString(ctx context.Context, reader io.Reader) string {
+	buf := new(strings.Builder)
+	_, err := io.Copy(buf, reader)
+	if err != nil {
+		log.Ctx(ctx).Error().Err(err).Msg("Failed to read file content, setting it to empty")
+
+	}
+	return buf.String()
 }
