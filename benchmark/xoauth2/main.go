687100353f607656fa2d193b5726d7ceef7394e2
Zhi Qu <quzhi65222714@gmail.com>
2023-10-05 15:37:15 -0700

    Print out message

--- benchmark/xoauth2/main.go
+++ benchmark/xoauth2/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"context"
 	"crypto/tls"
+	"github.com/emersion/go-message/mail"
 	"github.com/quzhi1/go-imap"
 	"github.com/quzhi1/go-imap/client"
 	"github.com/quzhi1/go-sasl"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"io"
 	"os"
 	"slices"
+	"strings"
 )
 
 const (
@@ -83,7 +86,7 @@ func main() {
 		imap.FetchInternalDate,
 		section.FetchItem(),
 	}
-	messageChans := make(chan *imap.Message, 10)
+	messageChans := make(chan *imap.Message, 100)
 	done := make(chan error, 1)
 	go func() {
 		done <- imapClient.UidFetch(seqset, items, messageChans)
@@ -93,68 +96,71 @@ func main() {
 	}
 
 	// Print the messages
-	//for msg := range messageChans {
-	//	if msg == nil {
-	//		log.Ctx(ctx).Fatal().Msg("Server didn't returned message")
-	//	}
-	//
-	//	r := msg.GetBody(&section)
-	//	if r == nil {
-	//		log.Ctx(ctx).Fatal().Msg("Server didn't returned message body")
-	//	}
-	//
-	//	// Create a new mail reader
-	//	mr, err := mail.CreateReader(r)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//
-	//	// Print some info about the message
-	//	log.Ctx(ctx).Info().Msgf("MessageId: %s", msg.Envelope.MessageId)
-	//	log.Ctx(ctx).Info().Msgf("Date: %s", msg.Envelope.Date)
-	//	log.Ctx(ctx).Info().Msgf("From: %v", msg.Envelope.From)
-	//	log.Ctx(ctx).Info().Msgf("Sender: %v", msg.Envelope.Sender)
-	//	log.Ctx(ctx).Info().Msgf("To: %v", msg.Envelope.To)
-	//	log.Ctx(ctx).Info().Msgf("Cc: %v", msg.Envelope.Cc)
-	//	log.Ctx(ctx).Info().Msgf("Bcc: %v", msg.Envelope.Bcc)
-	//	log.Ctx(ctx).Info().Msgf("ReplyTo: %v", msg.Envelope.ReplyTo)
-	//	log.Ctx(ctx).Info().Msgf("Subject: %s", msg.Envelope.Subject)
-	//
-	//	log.Ctx(ctx).Info().Strs("flags", msg.Flags).Msg("List flags")
-	//
-	//	// Print internal date
-	//	log.Ctx(ctx).Info().Msgf("InternalDate: %s", msg.InternalDate)
-	//
-	//	// Process each message's part
-	//	for {
-	//		p, err := mr.NextPart()
-	//		if err == io.EOF {
-	//			break
-	//		} else if err != nil {
-	//			panic(err)
-	//		}
-	//
-	//		switch h := p.Header.(type) {
-	//		case *mail.InlineHeader:
-	//			log.Ctx(ctx).Info().Msgf("Got text Content-Type: %s", h.Header.Header.Get("Content-Type"))
-	//			// This is the message's text (can be plain-text or HTML)
-	//			// b, _ := io.ReadAll(p.Body)
-	//			// log.Ctx(ctx).Info().Msgf("Got text: %v", string(b))
-	//		case *mail.AttachmentHeader:
-	//			// This is an attachment
-	//			filename, _ := h.Filename()
-	//			log.Ctx(ctx).Info().Msgf("Got attachment: %v", filename)
-	//
-	//			// Print attachment
-	//			buf := new(strings.Builder)
-	//			_, err := io.Copy(buf, p.Body)
-	//			if err != nil {
-	//				panic(err)
-	//			}
-	//			// log.Ctx(ctx).Info().Msgf("Attachment: %s", buf.String())
-	//		}
-	//	}
-	//}
+	for msg := range messageChans {
+		if msg == nil {
+			log.Ctx(ctx).Fatal().Msg("Server didn't returned message")
+		}
+
+		r := msg.GetBody(&section)
+		if r == nil {
+			log.Ctx(ctx).Fatal().Msg("Server didn't returned message body")
+		}
+
+		// Create a new mail reader
+		mr, err := mail.CreateReader(r)
+		if err != nil {
+			panic(err)
+		}
+
+		// Print some info about the message
+		log.Ctx(ctx).Info().Msgf("MessageId: %s", msg.Envelope.MessageId)
+		log.Ctx(ctx).Info().Msgf("Date: %s", msg.Envelope.Date)
+		log.Ctx(ctx).Info().Msgf("From: %v", msg.Envelope.From)
+		log.Ctx(ctx).Info().Msgf("Sender: %v", msg.Envelope.Sender)
+		log.Ctx(ctx).Info().Msgf("To: %v", msg.Envelope.To)
+		log.Ctx(ctx).Info().Msgf("Cc: %v", msg.Envelope.Cc)
+		log.Ctx(ctx).Info().Msgf("Bcc: %v", msg.Envelope.Bcc)
+		log.Ctx(ctx).Info().Msgf("ReplyTo: %v", msg.Envelope.ReplyTo)
+		log.Ctx(ctx).Info().Msgf("Subject: %s", msg.Envelope.Subject)
+
+		log.Ctx(ctx).Info().Strs("flags", msg.Flags).Msg("List flags")
+
+		// Print internal date
+		log.Ctx(ctx).Info().Msgf("InternalDate: %s", msg.InternalDate)
+
+		// Process each message's part
+		for {
+			p, err := mr.NextPart()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				panic(err)
+			}
+
+			switch h := p.Header.(type) {
+			case *mail.InlineHeader:
+				log.Ctx(ctx).Info().Msgf("Got text Content-Type: %s", h.Header.Header.Get("Content-Type"))
+				// This is the message's text (can be plain-text or HTML)
+				b, _ := io.ReadAll(p.Body)
+				log.Ctx(ctx).Info().Msgf("Got text: %v", string(b))
+			case *mail.AttachmentHeader:
+				// This is an attachment
+				filename, _ := h.Filename()
+				log.Ctx(ctx).Info().Msgf("Got attachment: %v", filename)
+
+				// Print attachment
+				buf := new(strings.Builder)
+				_, err := io.Copy(buf, p.Body)
+				if err != nil {
+					panic(err)
+				}
+				// log.Ctx(ctx).Info().Msgf("Attachment: %s", buf.String())
+			}
+		}
+
+		// Let's just print one message
+		break
+	}
 
 	// Logout
 	err = imapClient.Logout()
